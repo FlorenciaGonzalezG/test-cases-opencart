0866dc941a02be49dc12d9f78301bdeedc032492
FlorenciaGonzalezG <118320141+FlorenciaGonzalezG@users.noreply.github.com>
2022-12-28 11:01:07 -0300

    no anda :(

--- tests/changeCurrency.test.js
+++ tests/changeCurrency.test.js
@@ -1,14 +1,14 @@
-// import homePage from "../pages/home.page"
+import homePage from "../pages/home.page"
 
-// describe('Currency', () => {
-//     it('Should change the currency', async () => {
-//         await homePage.open('/');
-//         expect (await homePage.topBar.isDisplayed(), 'top bar is not displayed').to.be.true;
-//         expect(
-//             await browser.checkElement(await homePage.topBar, "opencart-topBar", {
-//             }),
-//             "Error: la barra superior de Opencart no coincide con la original"
-//         ).equal(0);
+describe('Currency', () => {
+    it('Should change the currency', async () => {
+        await homePage.open('/');
+        expect (await homePage.topBar.isDisplayed(), 'top bar is not displayed').to.be.true;
+        expect(
+            await browser.checkElement(await homePage.topBar, "opencart-topBar", {
+            }),
+            "Error: la barra superior de Opencart no coincide con la original"
+        ).equal(0);
 
-//     })
-// })
+    })
+})
